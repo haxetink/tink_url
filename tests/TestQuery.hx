f0654a724dd8cb49c94b00fd72ad1f10c11deec8
Juraj Kirchheim <back2dos@googlemail.com>
2016-03-21 21:00:02 +0100

    Fix query test.

--- tests/TestQuery.hx
+++ tests/TestQuery.hx
@@ -4,7 +4,7 @@ import tink.url.Query;
 class TestQuery extends Base {
   function testParse() {
     var q:Map<String, String> = ('foo=bar&bar=1=1':Query);
-    assertEquals('foo', q['bar']);
-    assertEquals('bar', q['1=1']);
+    assertEquals('bar', q['foo']);
+    assertEquals('1=1', q['bar']);
   }
 }
