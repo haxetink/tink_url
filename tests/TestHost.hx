f18f91734d5e8aa668f5cb25091d0c8fc73ace90
Kevin Leung <kevinresol@gmail.com>
2017-08-29 13:41:53 +0800

    Make "hosts" readonly

--- tests/TestHost.hx
+++ tests/TestHost.hx
@@ -26,36 +26,42 @@ class TestHost extends Base {
   function testMultiIpv4() {
     var url:Url = 'http://example.com,foo.com/foo/bar';
     assertEquals('example.com', url.host);
-    assertEquals('example.com', url.hosts[0]);
-    assertEquals('foo.com', url.hosts[1]);
+    assertEquals('example.com', getHost(url.hosts, 0));
+    assertEquals('foo.com', getHost(url.hosts, 1));
     
     var url:Url = 'http://example.com:80,foo.com:81/foo/bar';
     assertEquals('example.com:80', url.host);
-    assertEquals('example.com:80', url.hosts[0]);
-    assertEquals('foo.com:81', url.hosts[1]);
+    assertEquals('example.com:80', getHost(url.hosts, 0));
+    assertEquals('foo.com:81', getHost(url.hosts, 1));
   }
   
   function testMultiIpv6() {
     var url:Url = 'http://[::1],[::2]/foo/bar';
     assertEquals('[::1]', url.host);
-    assertEquals('[::1]', url.hosts[0]);
-    assertEquals('[::2]', url.hosts[1]);
+    assertEquals('[::1]', getHost(url.hosts, 0));
+    assertEquals('[::2]', getHost(url.hosts, 1));
     
     var url:Url = 'http://[::1]:80,[::2]:81/foo/bar';
     assertEquals('[::1]:80', url.host);
-    assertEquals('[::1]:80', url.hosts[0]);
-    assertEquals('[::2]:81', url.hosts[1]);
+    assertEquals('[::1]:80', getHost(url.hosts, 0));
+    assertEquals('[::2]:81', getHost(url.hosts, 1));
   }
   
   function testMixed() {
     var url:Url = 'http://example.com,[::2]/foo/bar';
     assertEquals('example.com', url.host);
-    assertEquals('example.com', url.hosts[0]);
-    assertEquals('[::2]', url.hosts[1]);
+    assertEquals('example.com', getHost(url.hosts, 0));
+    assertEquals('[::2]', getHost(url.hosts, 1));
     
     var url:Url = 'http://example.com:80,[::2]:81/foo/bar';
     assertEquals('example.com:80', url.host);
-    assertEquals('example.com:80', url.hosts[0]);
-    assertEquals('[::2]:81', url.hosts[1]);
+    assertEquals('example.com:80', getHost(url.hosts, 0));
+    assertEquals('[::2]:81', getHost(url.hosts, 1));
+  }
+  
+  function getHost(hosts:Iterable<String>, i) {
+    var c = 0;
+    for(host in hosts) if(c++ == i) return host;
+    return null;
   }
 }
