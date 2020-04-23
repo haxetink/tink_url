36b11e1df0c663bf5e1c33edf13ee7b02db232b4
Juraj Kirchheim <back2dos@googlemail.com>
2020-04-23 13:32:25 +0200

    Allow checking portions for validity, but skip decoding errors.

--- src/tink/url/Portion.hx
+++ src/tink/url/Portion.hx
@@ -4,25 +4,36 @@ using tink.CoreApi;
 using StringTools;
 
 abstract Portion(String) {
-  
+
   public var raw(get, never):String;
     inline function get_raw()
       return this;
-      
+
+  public function isValid():Bool
+    return
+      this == null ||
+        try {
+          this.urlDecode();
+          true;
+        }
+        catch (e:Dynamic) false;
+
   public inline function new(v:String)
     this = v;
-    
+
   @:to function stringly():tink.Stringly
     return toString();
-      
+
   @:to public function toString()
-    return 
+    return
       if (this == null) null;
-      else this.urlDecode();
-    
+      else
+        try this.urlDecode()
+        catch (e:Dynamic) '';
+
   @:from static function ofString(s:String)
     return new Portion(if (s == null) '' else s.urlEncode());
-    
+
   #if macro
   @:to public function toExpr()
     return macro new tink.url.Portion($v{this});
--- tests/TestPortion.hx
+++ tests/TestPortion.hx
@@ -6,12 +6,18 @@ using StringTools;
 
 @:asserts
 class TestPortion extends Base {
-  
+
   public function testNumbers() {
-    
     var portion = new Portion('%205%30.7e%2B85');
     asserts.assert(Std.parseFloat(portion) == 50.7e85);
     return asserts.done();
   }
-  
+
+  public function testInvalid() {
+    var portion = new Portion('%80');
+    asserts.assert(!portion.isValid());
+    asserts.assert(portion == '');
+    return asserts.done();
+  }
+
 }
